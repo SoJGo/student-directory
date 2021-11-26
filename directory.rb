56674d8ae75b4cd36a029e902b6477480716543f
SophieGowen <sophie.j.gowen@gmail.com>
2021-11-26 18:25:09 +0000

    Add exercises to complete

--- directory.rb
+++ directory.rb
@@ -1,3 +1,22 @@
+# EXERCISES TO COMPLETE:
+# 1 Print 1. 2. 3. etc in front of students names using each_with_index()
+# 2 Modify to print only students starting with a specific letter
+# 3 Modify your program to print student only if name length < 12
+# 4 Rewrite each() using while or until loops
+# 5 Add more information, hobbies, country of birth, height etc
+# 6 Research the method center() of String class. Use to make output aligned
+# 7 Ask for input for name and cohort in input_students method. Consider:
+    # for empty values – add default
+	# convert string to symbol
+	# what if user makes a typo
+# 8 Change the way users are displayed – grouped by cohorts 
+    # (or any category from input?)
+# 9 Change to print “...we have 1 student” not students when only one student
+# 10 Find a method to replace chomp(), it will require passing arguments
+# 11 Typo finding exercise – see branch exercise_11
+# 12 Only print the list if there is at least one student
+# ----------
+
 # create method to get input of names
 def input_students
     puts "Enter a students name, then press enter"
@@ -22,7 +41,7 @@ def print_header
 end
 
 def print(students)
-    students.each do |student| 
+    students.each_with_index do |student, index| 
         puts "#{student[:name]} (#{student[:cohort]} cohort)"
     end
 end
